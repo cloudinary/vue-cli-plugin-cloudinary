06920b3ade1dba04addb66db969060bd940bf403
Maya Shavin <maya@cloudinary.com>
2019-11-06 17:08:17 +0200

    UI

--- ui.js
+++ ui.js
@@ -9,6 +9,12 @@ module.exports = api => {
         onRead: () => ({
             prompts: require(resolve('./prompts'))
         }),
-        onWrite: () => {}
+        // onWrite: ({ api, prompts }) => {
+        //     const result = {}
+        //     for (const prompt of prompts) {
+        //         result[`${prompt.id}`] = await api.getAnswer(prompt.id)
+        //     }
+        //     api.setData('myConfig', result)
+        // }
     })
 }
