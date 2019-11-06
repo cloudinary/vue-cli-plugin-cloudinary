232f352a59b69dbd8fe59963ca55a485b07d09d8
Maya Shavin <maya@cloudinary.com>
2019-11-06 17:06:36 +0200

    add UI

--- ui.js
+++ ui.js
@@ -1,2 +1,14 @@
 /** main UI */
-module.exports = () => {}
+module.exports = api => {
+    api.describeConfig({
+        id: 'com.cloudinary',
+        name: 'Cloudinary SDK configuration for Vue',
+        description: 'Component setup',
+        link: 'https://cloudinary.com',
+        icon: '/_plugin/vue-cli-plugin-cloudinary/logo.png',
+        onRead: () => ({
+            prompts: require(resolve('./prompts'))
+        }),
+        onWrite: () => {}
+    })
+}
